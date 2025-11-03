0673d693e22ee512258ae7dafb9a7d195bf44691
ntorga <northontorga@gmail.com>
2025-11-03 12:15:24 -0300

    feat: allow unsafe chars via bool

--- src/domain/valueObject/unixFileName_test.go
+++ src/domain/valueObject/unixFileName_test.go
@@ -5,27 +5,27 @@ import (
 )
 
 func TestNewUnixFileName(t *testing.T) {
-	t.Run("StringInput", func(t *testing.T) {
+	t.Run("StringInputStrict", func(t *testing.T) {
 		testCaseStructs := []struct {
 			inputValue     any
 			expectedOutput UnixFileName
 			expectError    bool
 		}{
-			{"17795713_1253219528108045_4440713319482755723_n\\ \\(1\\).png", UnixFileName("17795713_1253219528108045_4440713319482755723_n\\ \\(1\\).png"), false},
+			{"17795713_1253219528108045_4440713319482755723_n (1).png", UnixFileName("17795713_1253219528108045_4440713319482755723_n (1).png"), false},
 			{"hello.php", UnixFileName("hello.php"), false},
 			{"hello_file.php", UnixFileName("hello_file.php"), false},
-			{"hello\\$_file.php", UnixFileName("hello\\$_file.php"), false},
 			{"hello (file).php", UnixFileName("hello (file).php"), false},
 			{"Imagem - Sem Título.jpg", UnixFileName("Imagem - Sem Título.jpg"), false},
-			{"Imagem - Sem Título & BW.jpg", UnixFileName("Imagem - Sem Título & BW.jpg"), false},
-			{"Imagem - Sem Título # BW.jpg", UnixFileName("Imagem - Sem Título # BW.jpg"), false},
-			{"Imagem - Sem Título @ BW.jpg", UnixFileName("Imagem - Sem Título @ BW.jpg"), false},
-			{"Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf", UnixFileName("Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf"), false},
 			{".sudo_as_admin_successful", UnixFileName(".sudo_as_admin_successful"), false},
 			{"WhatsApp Image 2018-06-22 at 18.05.08.jpeg", UnixFileName("WhatsApp Image 2018-06-22 at 18.05.08.jpeg"), false},
 			{123, UnixFileName("123"), false},
 			{true, UnixFileName("true"), false},
 			// Invalid file names
+			{"Imagem - Sem Título & BW.jpg", UnixFileName("Imagem - Sem Título & BW.jpg"), true},
+			{"Imagem - Sem Título # BW.jpg", UnixFileName("Imagem - Sem Título # BW.jpg"), true},
+			{"Imagem - Sem Título @ BW.jpg", UnixFileName("Imagem - Sem Título @ BW.jpg"), true},
+			{"hello$_file.php", UnixFileName("hello$_file.php"), true},
+			{"Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf", UnixFileName("Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf"), true},
 			{"", UnixFileName(""), true},
 			{".", UnixFileName(""), true},
 			{"..", UnixFileName(""), true},
@@ -39,7 +39,54 @@ func TestNewUnixFileName(t *testing.T) {
 		}
 
 		for _, testCase := range testCaseStructs {
-			actualOutput, conversionErr := NewUnixFileName(testCase.inputValue)
+			actualOutput, conversionErr := NewUnixFileName(testCase.inputValue, false)
+			if testCase.expectError && conversionErr == nil {
+				t.Errorf("MissingExpectedError: [%v]", testCase.inputValue)
+			}
+			if !testCase.expectError && conversionErr != nil {
+				t.Errorf("UnexpectedError: '%s' [%v]", conversionErr.Error(), testCase.inputValue)
+			}
+			if !testCase.expectError && actualOutput != testCase.expectedOutput {
+				t.Errorf("UnexpectedOutputValue: '%v' vs '%v' [%v]", actualOutput, testCase.expectedOutput, testCase.inputValue)
+			}
+		}
+	})
+
+	t.Run("StringInputUnsafe", func(t *testing.T) {
+		testCaseStructs := []struct {
+			inputValue     any
+			expectedOutput UnixFileName
+			expectError    bool
+		}{
+			{"17795713_1253219528108045_4440713319482755723_n (1).png", UnixFileName("17795713_1253219528108045_4440713319482755723_n (1).png"), false},
+			{"hello.php", UnixFileName("hello.php"), false},
+			{"hello_file.php", UnixFileName("hello_file.php"), false},
+			{"hello$_file.php", UnixFileName("hello$_file.php"), false},
+			{"hello (file).php", UnixFileName("hello (file).php"), false},
+			{"Imagem - Sem Título.jpg", UnixFileName("Imagem - Sem Título.jpg"), false},
+			{"Imagem - Sem Título & BW.jpg", UnixFileName("Imagem - Sem Título & BW.jpg"), false},
+			{"Imagem - Sem Título # BW.jpg", UnixFileName("Imagem - Sem Título # BW.jpg"), false},
+			{"Imagem - Sem Título @ BW.jpg", UnixFileName("Imagem - Sem Título @ BW.jpg"), false},
+			{"Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf", UnixFileName("Clean Architecture A Craftsman's Guide to Software Structure and Design.pdf"), false},
+			{".sudo_as_admin_successful", UnixFileName(".sudo_as_admin_successful"), false},
+			{"WhatsApp Image 2018-06-22 at 18.05.08.jpeg", UnixFileName("WhatsApp Image 2018-06-22 at 18.05.08.jpeg"), false},
+			{123, UnixFileName("123"), false},
+			{true, UnixFileName("true"), false},
+			{"file.php?blabla", UnixFileName("file.php?blabla"), false},
+			// Invalid file names
+			{"", UnixFileName(""), true},
+			{".", UnixFileName(""), true},
+			{"..", UnixFileName(""), true},
+			{"/", UnixFileName(""), true},
+			{"\\", UnixFileName(""), true},
+			{"@<php52.sandbox.ntorga.com>.php", UnixFileName(""), true},
+			{"../file.php", UnixFileName(""), true},
+			{"hello10/info.php", UnixFileName(""), true},
+			{[]string{"hello.php"}, UnixFileName(""), true},
+		}
+
+		for _, testCase := range testCaseStructs {
+			actualOutput, conversionErr := NewUnixFileName(testCase.inputValue, true)
 			if testCase.expectError && conversionErr == nil {
 				t.Errorf("MissingExpectedError: [%v]", testCase.inputValue)
 			}
