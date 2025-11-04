e05ae1c572d61f17c51c9ef988176b316da9bad7
ntorga <northontorga@gmail.com>
2025-11-04 19:00:28 -0300

    feat: add CertPairFilePathsReader

---
+++ src/infra/readThrough.go
@@ -0,0 +1,84 @@
+package tkInfra
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	tkValueObject "github.com/goinfinite/tk/src/domain/valueObject"
+)
+
+// Provides methods for reading information that when not found, are generated on the fly.
+type ReadThrough struct {
+}
+
+// Attempts to retrieve the certificate pair file paths from the environment variables
+// "CERTIFICATE_PAIR_CERT_PATH" and "CERTIFICATE_PAIR_KEY_PATH", otherwise generates a
+// self-signed certificate pair on local 'pki' directory and returns the absolute paths to
+// the generated files.
+func (rt *ReadThrough) CertPairFilePathsReader() (
+	certPath tkValueObject.UnixAbsoluteFilePath,
+	keyPath tkValueObject.UnixAbsoluteFilePath,
+	err error,
+) {
+	allowUnsafeFilePathChars := false
+
+	isCertPathValid := false
+	rawCertPath := os.Getenv("CERTIFICATE_PAIR_CERT_PATH")
+	if rawCertPath != "" {
+		certPath, err = tkValueObject.NewUnixAbsoluteFilePath(rawCertPath, allowUnsafeFilePathChars)
+		if err != nil {
+			return certPath, keyPath, errors.New("InvalidCertificatePairCertPath")
+		}
+		isCertPathValid = true
+	}
+
+	isKeyPathValid := false
+	rawKeyPath := os.Getenv("CERTIFICATE_PAIR_KEY_PATH")
+	if rawKeyPath != "" {
+		keyPath, err = tkValueObject.NewUnixAbsoluteFilePath(rawKeyPath, allowUnsafeFilePathChars)
+		if err != nil {
+			return certPath, keyPath, errors.New("InvalidCertificatePairKeyPath")
+		}
+		isKeyPathValid = true
+	}
+
+	if isCertPathValid && isKeyPathValid {
+		return certPath, keyPath, nil
+	}
+
+	synthesizer := Synthesizer{}
+	selfSignedCertPem, selfSignedKeyPem, err := synthesizer.SelfSignedCertificatePairPemFactory(nil, nil)
+	if err != nil {
+		return certPath, keyPath, err
+	}
+
+	fileClerk := FileClerk{}
+	pkiDir := "pki"
+	err = fileClerk.CreateDir(pkiDir)
+	if err != nil {
+		return certPath, keyPath, err
+	}
+
+	rawCertPath, err = filepath.Abs(pkiDir + "/cert.pem")
+	if err != nil {
+		return certPath, keyPath, errors.New("RetrieveSelfSignedCertAbsolutePathFailed")
+	}
+	certPath, err = tkValueObject.NewUnixAbsoluteFilePath(rawCertPath, allowUnsafeFilePathChars)
+	if err != nil {
+		return certPath, keyPath, errors.New("SelfSignedCertPathInvalid")
+	}
+	fileClerk.UpdateFileContent(certPath.String(), selfSignedCertPem, true)
+
+	rawKeyPath, err = filepath.Abs(pkiDir + "/key.pem")
+	if err != nil {
+		return certPath, keyPath, errors.New("RetrieveSelfSignedKeyAbsolutePathFailed")
+	}
+	keyPath, err = tkValueObject.NewUnixAbsoluteFilePath(rawKeyPath, allowUnsafeFilePathChars)
+	if err != nil {
+		return certPath, keyPath, errors.New("SelfSignedKeyPathInvalid")
+	}
+	fileClerk.UpdateFileContent(keyPath.String(), selfSignedKeyPem, true)
+
+	return certPath, keyPath, nil
+}
