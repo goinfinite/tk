4f8e43abad67b522f82e11cd0d85b11a8ef94d97
ntorga <northontorga@gmail.com>
2025-11-07 17:04:03 -0300

    feat: add PaginationQueryBuilder

---
+++ src/infra/db/paginationQueryBuilder.go
@@ -0,0 +1,57 @@
+package tkInfraDb
+
+import (
+	"errors"
+	"math"
+	"strings"
+
+	tkDto "github.com/goinfinite/tk/src/domain/dto"
+	"github.com/iancoleman/strcase"
+	"gorm.io/gorm"
+)
+
+func PaginationQueryBuilder(
+	dbQuery *gorm.DB,
+	requestPagination tkDto.Pagination,
+) (paginatedQuery *gorm.DB, responsePagination tkDto.Pagination, err error) {
+	var itemsTotal int64
+	err = dbQuery.Count(&itemsTotal).Error
+	if err != nil {
+		return paginatedQuery, responsePagination, errors.New("CountItemsTotalError: " + err.Error())
+	}
+
+	paginatedQuery = dbQuery.Limit(int(requestPagination.ItemsPerPage))
+	switch requestPagination.LastSeenId {
+	case nil:
+		offset := int(requestPagination.PageNumber) * int(requestPagination.ItemsPerPage)
+		paginatedQuery = paginatedQuery.Offset(offset)
+	default:
+		paginatedQuery = paginatedQuery.Where("id > ?", requestPagination.LastSeenId.String())
+	}
+
+	if requestPagination.SortBy != nil {
+		orderStatement := requestPagination.SortBy.String()
+		orderStatement = strings.ToLower(orderStatement)
+		orderStatement = strcase.ToSnake(orderStatement)
+
+		if requestPagination.SortDirection != nil {
+			orderStatement += " " + requestPagination.SortDirection.String()
+		}
+
+		paginatedQuery = paginatedQuery.Order(orderStatement)
+	}
+
+	itemsTotalUint := uint64(itemsTotal)
+	pagesTotal := uint32(
+		math.Ceil(float64(itemsTotal) / float64(requestPagination.ItemsPerPage)),
+	)
+
+	return paginatedQuery, tkDto.Pagination{
+		PageNumber:    requestPagination.PageNumber,
+		ItemsPerPage:  requestPagination.ItemsPerPage,
+		SortBy:        requestPagination.SortBy,
+		SortDirection: requestPagination.SortDirection,
+		PagesTotal:    &pagesTotal,
+		ItemsTotal:    &itemsTotalUint,
+	}, nil
+}
