499dc783a4b05d94417596c80b50c8387ed95063
ntorga <northontorga@gmail.com>
2026-01-07 13:06:49 -0300

    feat: normalize hostnames and improve phone parsing

--- src/domain/valueObject/url.go
+++ src/domain/valueObject/url.go
@@ -9,7 +9,7 @@ import (
 	tkVoUtil "github.com/goinfinite/tk/src/domain/valueObject/util"
 )
 
-var urlRegex = regexp.MustCompile(`^(?:(?:mailto:)?(?P<mailUsername>[a-z0-9._%+-]+)@(?P<mailHostname>[a-z0-9.-]+\.[a-z]{2,})|(?:tel:)?(?P<phone>\+?\d{1,12}-?\d{0,12})|(?:(?P<scheme>(?:https?|wss?|grpcs?|tcp|udp|ftp|ftps|file|data|irc|imap|nntp|pop3|smtp|telnet):\/\/)?(?:(?P<userAuthUsername>[a-z0-9._~%!$&'()*+,;=-]+)(?::(?P<userAuthPassword>[a-z0-9._~%!$&'()*+,;=:-]+))?@)?(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*)(?::(?P<networkPort>\d{1,6}))?(?P<path>\/[A-Za-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?))$`)
+var urlRegex = regexp.MustCompile(`^(?:(?:mailto:)?(?P<mailUsername>[a-z0-9._%+-]+)@(?i:(?P<mailHostname>[a-z0-9.-]+\.[a-z]{2,}))|(?:tel:)?(?P<phone>\+?\d{6,15}(?:-\d{1,12})?)|(?:(?P<scheme>(?:https?|wss?|grpcs?|tcp|udp|ftp|ftps|file|data|irc|imap|nntp|pop3|smtp|telnet):\/\/)?(?:(?P<userAuthUsername>[a-z0-9._~%!$&'()*+,;=-]+)(?::(?P<userAuthPassword>[a-z0-9._~%!$&'()*+,;=:-]+))?@)?(?i:(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*))(?::(?P<networkPort>\d{1,6}))?(?P<path>\/[A-Za-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?))$`)
 
 type Url string
 
@@ -36,6 +36,20 @@ func NewUrl(value any) (url Url, err error) {
 		if namedGroupsValuesMap["mailUsername"] != "" && !strings.HasPrefix(stringValue, "mailto:") {
 			stringValue = "mailto:" + stringValue
 		}
+	}
+
+	if namedGroupsValuesMap["hostname"] != "" {
+		lowercaseHostname := strings.ToLower(namedGroupsValuesMap["hostname"])
+		stringValue = strings.ReplaceAll(
+			stringValue, namedGroupsValuesMap["hostname"], lowercaseHostname,
+		)
+	}
+
+	if namedGroupsValuesMap["mailHostname"] != "" {
+		lowercaseMailHostname := strings.ToLower(namedGroupsValuesMap["mailHostname"])
+		stringValue = strings.ReplaceAll(
+			stringValue, namedGroupsValuesMap["mailHostname"], lowercaseMailHostname,
+		)
 	}
 
 	if namedGroupsValuesMap["networkPort"] != "" {
