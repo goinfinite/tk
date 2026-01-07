728c58813fd73c86fcfa6bed19e2bd08698a0157
ntorga <northontorga@gmail.com>
2026-01-07 11:01:21 -0300

    feat: allow different schemes on url value object

--- src/domain/valueObject/url.go
+++ src/domain/valueObject/url.go
@@ -3,12 +3,11 @@ package tkValueObject
 import (
 	"errors"
 	"regexp"
-	"strings"
 
 	tkVoUtil "github.com/goinfinite/tk/src/domain/valueObject/util"
 )
 
-var urlRegex = regexp.MustCompile(`^(?P<schema>https?:\/\/)(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*)(:(?P<port>\d{1,6}))?(?P<path>\/[A-Za-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?$`)
+var urlRegex = regexp.MustCompile(`^(?P<scheme>(https?|wss?|grpcs?|tcp|udp|ftp|mailto|file|data|irc):\/\/)?(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*)(:(?P<port>\d{1,6}))?(?P<path>\/[A-Za-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?$`)
 
 type Url string
 
@@ -18,12 +17,13 @@ func NewUrl(value any) (url Url, err error) {
 		return url, errors.New("UrlValueMustBeString")
 	}
 
-	if !strings.HasPrefix(stringValue, "http") {
-		stringValue = "https://" + stringValue
-	}
-
 	if !urlRegex.MatchString(stringValue) {
 		return url, errors.New("InvalidUrl")
+	}
+
+	namedGroupsValuesMap := tkVoUtil.NamedGroupsExtractor(urlRegex, stringValue)
+	if namedGroupsValuesMap["scheme"] == "" {
+		stringValue = "https://" + stringValue
 	}
 
 	return Url(stringValue), nil
