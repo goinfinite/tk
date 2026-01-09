eb89f8de3b812c7b50df7d8385eabe4590b553bb
ntorga <northontorga@gmail.com>
2026-01-09 17:58:53 -0300

    fix: remove redundant component count validation

--- src/domain/valueObject/x509PolicyOid.go
+++ src/domain/valueObject/x509PolicyOid.go
@@ -3,7 +3,6 @@ package tkValueObject
 import (
 	"errors"
 	"regexp"
-	"strings"
 
 	tkVoUtil "github.com/goinfinite/tk/src/domain/valueObject/util"
 )
@@ -20,11 +19,6 @@ func NewX509PolicyOID(value any) (oid X509PolicyOID, err error) {
 
 	if !x509PolicyOidRegex.MatchString(stringValue) {
 		return oid, errors.New("InvalidX509PolicyOID")
-	}
-
-	components := strings.Split(stringValue, ".")
-	if len(components) < 2 {
-		return oid, errors.New("InvalidX509PolicyOIDTooFewComponents")
 	}
 
 	return X509PolicyOID(stringValue), nil
